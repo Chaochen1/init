8b37914e55cf687e71a5f7420ba48b4171b549b5
Beyond <1335334316@qq.com>
2019-10-24 19:39:35 +0800

    init

---
+++ src/main/java/com/hohai/maven/App.java
@@ -0,0 +1,13 @@
+package com.hohai.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
